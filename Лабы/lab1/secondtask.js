d9607f74bf8ed4f074b3d11f1b9be7d9e838940f
grisha9154 <grisha9154@gmail.com>
2017-09-06 02:09:29 +0300

    need fix trubl slesh

--- Лабы/lab1/secondtask.js
+++ Лабы/lab1/secondtask.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 var filePath = process.argv[2];
+var scriptValue = "const fs = require('fs');var tabs = ' ';var path = '"+filePath+"';function getAllDir(dirPath,tab,otPath) {fs.readdir(dirPath,function (err, files) {if(!err){for (var i=0;i<files.length;i++){console.log(tab+otPath+files[i]);getAllDir(dirPath+'\\'+files[i],tab+' ',files[i]+'\\');}}});}getAllDir(path,tabs,'');";
 filePath+="\\summary.js";
 
-
+fs.open(filePath,'w+',0644,function (err, file_handler) {
+    if(!err){
+        fs.write(file_handler,scriptValue,null,'ascii',function (err, writer) {
+            if(!err){
+                console.log('Всё записанно');
+            }
+            else{
+                console.log('Ошибка при записи');
+            }
+        })
+    }
+    else{
+        console.log('Ошибка открытия файла');
+    }
+})
 
 console.log(filePath);
