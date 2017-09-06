a9856d329082633d90a78bee3a705c110529f396
grisha9154 <grisha9154@gmail.com>
2017-09-06 18:31:48 +0300

    Fiend all txt fiels

--- Лабы/lab1/secondtask.js
+++ Лабы/lab1/secondtask.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+let fs = require('fs');
 var filePath = process.argv[2];
 var scriptValue = "const fs = require('fs');var tabs = ' ';var path = '"+process.argv[2]+"';function getAllDir(dirPath,tab,otPath) {fs.readdir(dirPath,function (err, files) {if(!err){for (var i=0;i<files.length;i++){console.log(tab+otPath+files[i]);getAllDir(dirPath+'\\\\'+files[i],tab+' ',files[i]+'\\\\');}}});}getAllDir(path,tabs,'');";
-filePath+="/summary.js";
+filePath+="\\summary.js";
 
-fs.open(filePath,'w+',0644,function (err, file_handler) {
+fs.open(filePath,'w+',function (err, file_handler) {
     if(!err){
         fs.write(file_handler,scriptValue,null,'utf-8',function (err, writer) {
             if(!err){
@@ -13,12 +12,31 @@ fs.open(filePath,'w+',0644,function (err, file_handler) {
             else{
                 console.log('Ошибка при записи');
             }
-            fs.mkdir(process.argv[2]+'\\summary');
+            getAllDir(process.argv[2]);
+            //  fs.mkdir(process.argv[2]+'\\summary');
         })
     }
     else{
         console.log('Ошибка открытия файла');
     }
+
 });
 
-console.log(filePath);
+function getAllDir(dirPath) {
+    fs.readdir(dirPath, function (err, files) {
+        if (!err) {
+            for (let i = 0; i < files.length; i++) {
+                fs.stat(dirPath+"\\"+files[i], function (err, stats) {
+                    if(stats.isDirectory()){
+                        getAllDir(dirPath + '\\' + files[i]);
+                    }
+                    else{
+                        if(stats.isFile() && files[i].indexOf(".txt")+1){
+                            console.log(files[i]);
+                        }
+                    }
+                })
+            }
+        }
+    });
+}
