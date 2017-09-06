f05c00ac5c7757d6923b49926c7364810fdba169
grisha9154 <grisha9154@gmail.com>
2017-09-06 08:49:33 +0300

    Fix some trable

--- Лабы/lab1/secondtask.js
+++ Лабы/lab1/secondtask.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 var filePath = process.argv[2];
-var scriptValue = "const fs = require('fs');var tabs = ' ';var path = '"+filePath+"';function getAllDir(dirPath,tab,otPath) {fs.readdir(dirPath,function (err, files) {if(!err){for (var i=0;i<files.length;i++){console.log(tab+otPath+files[i]);getAllDir(dirPath+'\\'+files[i],tab+' ',files[i]+'\\');}}});}getAllDir(path,tabs,'');";
+var scriptValue = "const fs = require('fs');var tabs = ' ';var path = '"+process.argv[2]+"';function getAllDir(dirPath,tab,otPath) {fs.readdir(dirPath,function (err, files) {if(!err){for (var i=0;i<files.length;i++){console.log(tab+otPath+files[i]);getAllDir(dirPath+'\\\\'+files[i],tab+' ',files[i]+'\\\\');}}});}getAllDir(path,tabs,'');";
 filePath+="\\summary.js";
 
 fs.open(filePath,'w+',0644,function (err, file_handler) {
     if(!err){
-        fs.write(file_handler,scriptValue,null,'ascii',function (err, writer) {
+        fs.write(file_handler,scriptValue,null,'utf-8',function (err, writer) {
             if(!err){
                 console.log('Всё записанно');
             }
